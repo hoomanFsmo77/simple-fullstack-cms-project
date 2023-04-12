3495d25fcf155dc0b7d7c0a6e5356123bd1fa4f3
hooman mousavi <hoomanmousavi77@gmail.com>
2023-04-12 23:25:11 +0430

    initial commit

---
+++ client/nuxt.config.ts
@@ -0,0 +1,25 @@
+
+export default defineNuxtConfig({
+    build:{
+        transpile:[]
+    },
+    css:['~/assets/style/App.scss'],
+    app:{
+        rootId:'v-app',
+        rootTag:'main',
+        head:{
+            title:'my website',
+            meta: [
+                { name: 'viewport', content: 'width=device-width ,initial-scale=1.0' },
+                { name: 'description', content: 'welcome to My project' },
+                { name: 'keyword', content: 'HTML,CSS,Js developer' },
+                { "http-equiv": 'X-UA-Compatible', content: 'ie=edge' },
+            ],
+            bodyAttrs:{}
+        }
+    },
+    srcDir: './src',
+    modules: [
+        '@pinia/nuxt','@nuxt/image-edge'
+    ],
+})
